e121abdf910d33b3c96b41f686c99f6773470335
Brian Phiri <brian.phiri@bosch.com>
2020-02-25 01:23:21 +0300

    format response

--- gossip/src/main/java/com/brianphiri/gossip/send.kt
+++ gossip/src/main/java/com/brianphiri/gossip/send.kt
@@ -3,10 +3,10 @@ package com.brianphiri.gossip
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
-fun send(req: com.brianphiri.gossip.Request) {
+fun send(req: String) {
     val client = OkHttpClient()
     val request = Request.Builder().url("ws://192.168.0.23:8080/ws").build()
-    client.newWebSocket(request, listener(req.toString()))
+    client.newWebSocket(request, listener(req))
     client.dispatcher().executorService().shutdown()
 }
 
--- gossip/src/main/java/com/brianphiri/gossip/whisper.kt
+++ gossip/src/main/java/com/brianphiri/gossip/whisper.kt
@@ -1,12 +1,20 @@
 package com.brianphiri.gossip
 
-fun whisper(resource: String, tag: String="", body : Map<String, Any> = mapOf(), metadata : Map<String, Any> = mapOf()){
+import com.google.gson.Gson
+import org.json.JSONObject
+
+fun whisper(resource: String, tag: String="", b : Map<String, Any> = mapOf(), m : Map<String, Any> = mapOf()){
 
     val r = Request()
+    val body = JSONObject(b).toString()
+    val metadata = JSONObject(m).toString()
     r.resource = resource
     r.tag = tag
-    r.body = body.toString()
-    r.metadata = metadata.toString()
+    r.body = body
+    r.metadata = metadata
 
-    send(r)
+
+    val jsonString = Gson().toJson(r)
+
+    send(jsonString)
 }
