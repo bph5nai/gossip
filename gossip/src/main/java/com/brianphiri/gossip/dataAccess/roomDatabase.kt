ac89beee4da0ed5fd398bef41b8948de8c934da9
Brian Phiri <brian.phiri@bosch.com>
2020-02-29 08:31:00 +0300

    added offline support.

--- gossip/src/main/java/com/brianphiri/gossip/dataAccess/roomDatabase.kt
+++ gossip/src/main/java/com/brianphiri/gossip/dataAccess/roomDatabase.kt
@@ -5,24 +5,30 @@ import com.brianphiri.gossip.dataSource.room.EventLogs
 import com.brianphiri.gossip.dataSource.room.InternalDatabase
 import com.brianphiri.gossip.dataSource.send
 import com.brianphiri.gossip.util.isOnline
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 fun writeToInternalDatabase(context: Context, data: String) {
-    val dao = InternalDatabase(context).eventLogsDao()
-    val request = EventLogs()
-    request.report = data
-    dao.recordLog(request)
+    GlobalScope.launch {
+        val dao = InternalDatabase(context).eventLogsDao()
+        val request = EventLogs()
+        request.report = data
+        dao.recordLog(request)
+    }
 }
 
 fun checkIfDatabaseHasData(context: Context) {
-    val dao = InternalDatabase(context).eventLogsDao()
-    val logs = dao.getAllLogs()
-    if(logs.isNotEmpty()){
-        for (i in logs) {
-            if(isOnline(context)){
-                send(i.report)
-                dao.removeLog(i.id)
-            } else {
-                break
+    GlobalScope.launch {
+        val dao = InternalDatabase(context).eventLogsDao()
+        val logs = dao.getAllLogs()
+        if(logs.isNotEmpty()){
+            for (i in logs) {
+                if(isOnline(context)){
+                    send(i.report)
+                    dao.removeLog(i.id)
+                } else {
+                    break
+                }
             }
         }
     }
